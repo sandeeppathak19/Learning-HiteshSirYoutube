84cfc6abadb020888f5d38ed38f3252b0fe15de7
SANDEEP PATHAK <sandeep11393pathak@gmail.com>
2025-02-24 11:21:10 +0000

    3 topic

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,14 @@
+let score=10 //number
+let scoreValue= "20" //String
+let scoreNumber="20abc" //NaN- Not a Number
+
+let valueToNumber=Number(scoreValue)
+let valueToNumber2=Number(scoreNumber)
+
+console.log(typeof score);
+console.log(typeof score);
+console.log(typeof score);
+console.log(typeof score);
+
+
+
