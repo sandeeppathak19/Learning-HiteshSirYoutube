2c70ff12e187e94ee6ce36ef306767bec8eb1750
SANDEEP PATHAK <sandeep11393pathak@gmail.com>
2025-02-24 08:30:42 +0000

    contaner save changes

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId=14456
+let accountEmail="sandeep@gmail.com"
+var accountPasskey="12345"
+accountCity="jaipur"
+let accountState;
+
+// accountId=2// Not allowed
+// accountEmail="hdfc@gmail.com"
+// accountPasskey="141414"
+// accountCity="Bengaluru"
+
+
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPasskey,accountCity,accountState]);
