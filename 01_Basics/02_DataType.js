fdb91f4313f35b2fadbb5fb8cb592d9099118d71
SANDEEP PATHAK <sandeep11393pathak@gmail.com>
2025-02-24 11:15:26 +0000

    done

---
+++ 01_Basics/02_DataType.js
@@ -0,0 +1,32 @@
+/*
+Primitive Data Type:-
+number
+int
+bigint
+string
+boolen
+symbols
+
+Object Data type:-
+typeof 
+lenof
+*/
+
+const userName= "Sandeep pathak"
+let userAge=30
+let userId=12345
+let userEmail="sandeep@gmail.com"
+let userCity
+let userState=null
+let userLogin=false
+
+// console.table([userName,userAge,userId,userEmail,userCity,userState,userLogin]);
+
+// console.table(typeof [userName,userAge,userId,userEmail,userCity,userState,userLogin]);
+console.log(typeof userName);
+console.log(typeof userAge);
+// console.log(typeof userId);
+console.log(typeof userEmail);
+console.log(typeof userCity);
+console.log(typeof userState);
+console.log(typeof userLogin);
