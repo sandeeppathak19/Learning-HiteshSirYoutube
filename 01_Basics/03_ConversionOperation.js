e6e476b0f6acc216ed01c216329246c7977d7715
SANDEEP PATHAK <sandeep11393pathak@gmail.com>
2025-02-24 12:05:57 +0000

    done

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -1,25 +1,53 @@
-let score=10 //number
-let scoreValue= "20" //String
-let scoreNumber="20abc" //NaN- Not a Number
+// let score=10 //number
+// let scoreValue= "20" //String
+// let scoreNumber="20abc" //NaN- Not a Number
 
-let valueToNumber=Number(scoreValue)
-let valueToNumber2=Number(scoreNumber)
+// let valueToNumber=Number(scoreValue)
+// let valueToNumber2=Number(scoreNumber)
 
-console.log(score);
-console.log(typeof score);
+// console.log(score);
+// console.log(typeof score);
 
-console.log(scoreValue);
-console.log(typeof scoreValue);
+// console.log(scoreValue);
+// console.log(typeof scoreValue);
 
-console.log(scoreNumber);
-console.log(typeof scoreNumber);
+// console.log(scoreNumber);
+// console.log(typeof scoreNumber);
 
-console.log(valueToNumber);
-console.log(typeof valueToNumber);
+// console.log(valueToNumber);
+// console.log(typeof valueToNumber);
 
-console.log(valueToNumber2);
-console.log(typeof valueToNumber2);
+// console.log(valueToNumber2);
+// console.log(typeof valueToNumber2);
 
 
+
+
+let valueScore= null;
+let valueNo=Number(valueScore)
+console.log(valueScore)
+console.log(typeof valueScore)
+console.log(valueNo)
+console.log(typeof valueNo)
+
+
+let valueScore1= false; //Boolean value false
+let valueScore01= true; //Boolean value true
+
+console.log(valueScore1);
+console.log(typeof valueScore1)
+let valueNo1=Number(valueScore1)
+console.log(valueNo1)
+
+let valueNo22=Number(valueScore01)
+console.log(typeof valueScore01)
+console.log(valueScore01);
+console.log(valueNo22)
+
+
+
+// let valueNo11=Number(valueScore)
+// console.log(valueNo11)
+// console.log(typeof valueNo11)
 
 
