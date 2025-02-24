c4322925459931b0e6c549948f57a8925ec41b3e
SANDEEP PATHAK <sandeep11393pathak@gmail.com>
2025-02-24 11:30:57 +0000

    done

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -5,10 +5,21 @@ let scoreNumber="20abc" //NaN- Not a Number
 let valueToNumber=Number(scoreValue)
 let valueToNumber2=Number(scoreNumber)
 
+console.log(score);
 console.log(typeof score);
-console.log(typeof score);
-console.log(typeof score);
-console.log(typeof score);
+
+console.log(scoreValue);
+console.log(typeof scoreValue);
+
+console.log(scoreNumber);
+console.log(typeof scoreNumber);
+
+console.log(valueToNumber);
+console.log(typeof valueToNumber);
+
+console.log(valueToNumber2);
+console.log(typeof valueToNumber2);
+
 
 
 
